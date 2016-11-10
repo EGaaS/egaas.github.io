9e482d004d49cb9c53a598fa45a010f84375cc41
Yuriy <yuriy.lomakin@gmail.com>
2016-11-10 13:18:53 +0200

    update

--- src/www/js/app/app.js
+++ src/www/js/app/app.js
@@ -3,13 +3,18 @@
 	
 	angular.module('app', ['ui.router', 'pascalprecht.translate', 'ngCookies']);
 	
-	angular.module('app').run(function($rootScope, $location, $window, $timeout){
+	angular.module('app').run(function($rootScope, $location, $window, $timeout, $anchorScroll){
 		$rootScope.$on('$stateChangeSuccess', function(){
 			$window.scrollTo(0, 0);
 			
 			$rootScope.scroll = function(target){
 				$timeout(function() {
-					$.scrollTo("#" + target, 500);
+					$.scrollTo("#" + target, 500, {easing:'linear', onAfter: function() {
+						$timeout(function() {
+							//$location.hash(target);
+							//$anchorScroll();
+						}, 0);
+					}});
 				}, 0);
 			};
 			

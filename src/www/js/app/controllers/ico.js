7e7dbc0a70955b30481c84d72430b5a8bf59d442
Yuriy <yuriy.lomakin@gmail.com>
2016-11-13 14:59:49 +0200

    update ico

--- src/www/js/app/controllers/ico.js
+++ src/www/js/app/controllers/ico.js
@@ -6,6 +6,10 @@
 	function IcoCtrl($rootScope, $scope, $http, $location, $state){
 		var vm = this;
 		vm.ICO = {};
+		vm.limit = [];
+		vm.summ = [];
+		vm.sold = 0;
+		vm.total = 0;
 		vm.getICO = getICO;
 		
 		function getICO () {
@@ -16,6 +20,12 @@
 			.success(function(data){
 				console.log(data);
 				vm.ico = data;
+				for (var i = 0; i < data.stat.length; i++) {
+					vm.summ.push(parseInt(vm.ico.stat[i].forsell * vm.ico.stat[i].btc));
+					vm.limit.push(vm.ico.stat[i].forsell / 1000000);
+					vm.sold += vm.ico.stat[i].sold;
+					vm.total += vm.ico.stat[i].forsell;
+				}
 			});
 		}
 		

c1d7620ffa9a47b83674e523fa51b144146a1cda
Yuriy <yuriy.lomakin@gmail.com>
2016-11-19 09:11:56 +0200

    update prototypes

--- src/www/view/js/ico.js
+++ src/www/view/js/ico.js
@@ -100,16 +100,31 @@ $(function () {
     });
 	
 	jQuery("#nanoGallery").nanoGallery({
-		thumbnailWidth: '200 XS80 SM150 LA250 XL300',
-		thumbnailHeight: '200 XS80 SM150 LA250 XL300',
+		breadcrumbAutoHideTopLevel: true,
+		galleryToolbarHideIcons: true,
+		breakpointSizeSM : 720, breakpointSizeME : 960, breakpointSizeLA : 1000, breakpointSizeXL : 1200,
+		thumbnailWidth: '85 SM220 ME220 LA300 XL300',
+		thumbnailHeight: '71 SM183 ME183 LA250 XL250',
 		thumbnailLabel: {
-			display: false          
+			display:false,
+			align:'center',
+			position:'overImageOnMiddle'
+		},
+		thumbnailL1Label:{
+			display:false,
+			align:'center',
+			position:'overImageOnMiddle',
+			displayDescription: false,
+			hideIcons: true
 		},
 		maxItemsPerLine: 3,
 		paginationMaxLinesPerPage: 1,
 		paginationDots: true,
 		locationHash: false,
-		thumbnailHoverEffect:'borderLighter,imageScaleIn80',
+		imageTransition : 'slide',
+		touchAnimationL1: true,
+		touchAnimation:false,
+		thumbnailHoverEffect:[{'name':'imageScale150', 'duration':700},{'name':'labelAppear75', 'duration':400},{'name':'descriptionAppear', 'duration':1000}],
 		itemsBaseURL:'/images/'
 	});
 });

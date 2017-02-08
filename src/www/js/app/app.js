a22903d3ea9d249343a1bbf3627fcc8b2945563f
Yuriy <yuriy.lomakin@gmail.com>
2017-02-08 14:14:36 +0200

    test

--- src/www/js/app/app.js
+++ src/www/js/app/app.js
@@ -76,18 +76,19 @@
 		navigator.sayswho = (function(){
 			var ua = navigator.userAgent, tem, 
 			M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*([\d\.]+)/i) || [];
+			console.log(M);
 			if(/trident/i.test(M[1])){
 				tem = /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
 				return 'IE '+(tem[1] || '');
 			}
-			M = M[2]? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
+			M = M[2] ? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
 			if((tem = ua.match(/version\/([\.\d]+)/i)) !== null) {
 				M[2] = tem[1];
 				return M.join(' ');
 			}
 		})();
 		
-		console.log(navigator.sayswho);
+		//console.log(navigator.sayswho);
 	});
 	
 })();

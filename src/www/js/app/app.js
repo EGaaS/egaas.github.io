314725b6e4e2205923d14401231769ced123dff5
Yuriy <yuriy.lomakin@gmail.com>
2017-02-09 09:30:38 +0200

    fix

--- src/www/js/app/app.js
+++ src/www/js/app/app.js
@@ -17,8 +17,33 @@
 			}
 		}
 		
+		navigator.detectIE = (function(){
+			var ua = navigator.userAgent/*, tem*/, 
+			M = ua.match(/(opera|chrome|safari|firefox|msie|trident|edge(?=\/))\/?\s*([\d\.]+)/i) || [];
+			//console.log(M);
+			if (M[1].toLowerCase() === "msie" || M[1].toLowerCase() === "trident") {
+				return true;
+			}
+			/*if(/trident/i.test(M[1])){
+				tem = /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
+				return 'IE '+(tem[1] || '');
+			}
+			M = M[2] ? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
+			if((tem = ua.match(/version\/([\.\d]+)/i)) !== null) {
+				M[2] = tem[1];
+				return M.join(' ');
+			}*/
+		})();
+		
 		$rootScope.$on('$stateChangeStart', function(){
 			angular.element("body").removeClass("page_404");
+			
+			if (navigator.detectIE) {
+				$timeout(function() {
+					$state.go('IE');
+					angular.element("body").addClass("IE");
+				});
+			}
 		});
 		
 		$rootScope.$on('$viewContentLoading', function(){
@@ -75,31 +100,6 @@
 					}, 0);
 				}});
 			}, time);
-		}
-		
-		navigator.detectIE = (function(){
-			var ua = navigator.userAgent/*, tem*/, 
-			M = ua.match(/(opera|chrome|safari|firefox|msie|trident|edge(?=\/))\/?\s*([\d\.]+)/i) || [];
-			//console.log(M);
-			if (M[1].toLowerCase() === "msie" || M[1].toLowerCase() === "trident") {
-				return true;
-			}
-			/*if(/trident/i.test(M[1])){
-				tem = /\brv[ :]+(\d+(\.\d+)?)/g.exec(ua) || [];
-				return 'IE '+(tem[1] || '');
-			}
-			M = M[2] ? [M[1], M[2]]:[navigator.appName, navigator.appVersion, '-?'];
-			if((tem = ua.match(/version\/([\.\d]+)/i)) !== null) {
-				M[2] = tem[1];
-				return M.join(' ');
-			}*/
-		})();
-		
-		if (navigator.detectIE) {
-			$timeout(function() {
-				$state.go('IE');
-				document.body.className += "IE";
-			});
 		}
 	});
 	
